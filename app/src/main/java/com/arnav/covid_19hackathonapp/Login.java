6f5551dea41719ef72d4478b0804e2da95cfc378
arnavs-0 <>
2020-04-30 18:29:24 -0400

    finsihed login screen

---
+++ app/src/main/java/com/arnav/covid_19hackathonapp/Login.java
@@ -0,0 +1,16 @@
+package com.arnav.covid_19hackathonapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/arnav/covid_19hackathonapp/MainActivity.java
+++ app/src/main/java/com/arnav/covid_19hackathonapp/MainActivity.java
@@ -2,10 +2,13 @@ package com.arnav.covid_19hackathonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -46,9 +49,12 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, Dashboard.class);
-                startActivity(intent);
-                finish();
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                Pair[] pairs = new Pair[1];
+                pairs[0] = new Pair<View, String>(logo, "logo_image");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, new Pair<View, String>(logo, "logo_image"));
+                startActivity(intent, options.toBundle());
             }
         }, SPLASH_SCREEN);
 
