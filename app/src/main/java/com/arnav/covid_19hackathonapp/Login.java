1afba3c014ad06066929473fc8d7f8dbdb273dcb
arnavs-0 <>
2020-05-01 14:38:21 -0400

    signup form

--- app/src/main/java/com/arnav/covid_19hackathonapp/Login.java
+++ app/src/main/java/com/arnav/covid_19hackathonapp/Login.java
@@ -2,6 +2,7 @@ package com.arnav.covid_19hackathonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Pair;
@@ -11,11 +12,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class Login extends AppCompatActivity {
     Button callSignUp, callGo;
-    ImageView callLogo_image, callLogo_image_reg;
-    TextView logo_text, logo_desc;
-
+    ImageView image;
+    TextView logo_text, slogan_text;
+    TextInputLayout username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,14 @@ public class Login extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
+        //Hooks for Ids
         callSignUp = findViewById(R.id.signup_access);
+        callGo = findViewById(R.id.go);
+        image = findViewById(R.id.logo_image);
+        logo_text = findViewById(R.id.logo_name);
+        slogan_text = findViewById(R.id.slogan_name);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
 
 
         callSignUp.setOnClickListener(new View.OnClickListener() {
@@ -31,9 +41,18 @@ public class Login extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(Login.this, SignUp.class);
 
-                Pair[] pairs = new Pair[3];
+                Pair[] pairs = new Pair[7];
 
-                //pairs[0]
+                pairs[0] = new Pair<View, String>(image, "logo_image");
+                pairs[1] = new Pair<View, String>(logo_text, "logo_text");
+                pairs[2] = new Pair<View, String>(slogan_text, "logo desc");
+                pairs[3] = new Pair<View, String>(username, "username_tran");
+                pairs[4] = new Pair<View, String>(password, "password_tran");
+                pairs[5] = new Pair<View, String>(callGo, "go_tran");
+                pairs[6] = new Pair<View, String>(callSignUp, "login_signup_tran");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Login.this, pairs);
+                startActivity(intent, options.toBundle());
 
             }
         });
