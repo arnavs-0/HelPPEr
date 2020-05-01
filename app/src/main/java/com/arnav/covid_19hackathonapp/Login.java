f7a430550017251506c1955401de21ffc47b7a23
arnavs-0 <>
2020-04-30 21:44:28 -0400

    signup version 1

--- app/src/main/java/com/arnav/covid_19hackathonapp/Login.java
+++ app/src/main/java/com/arnav/covid_19hackathonapp/Login.java
@@ -2,15 +2,40 @@ package com.arnav.covid_19hackathonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    Button callSignUp, callGo;
+    ImageView callLogo_image, callLogo_image_reg;
+    TextView logo_text, logo_desc;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        callSignUp = findViewById(R.id.signup_access);
+
+
+        callSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, SignUp.class);
+
+                Pair[] pairs = new Pair[3];
+
+                //pairs[0]
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/arnav/covid_19hackathonapp/SignUp.java
@@ -0,0 +1,14 @@
+package com.arnav.covid_19hackathonapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
